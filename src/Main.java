e0c6c91913ef58c00a0d6d1daddcbf4a15b3f4b9
sapthaa <andre.saptha@hotmail.com>
2023-09-07 13:04:31 +0200

    First

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Primitive//
+        byte age = 30;
+        long viewCounts = 123_3456_671_23L;
+        float price = 10.99F;
+        char letter = 'a';
+        short temperature = 20;
+        boolean isEligable = true;
+        int meeting = 20;
+        double decimal = 1.11;
+
+
+        Point point1 = new Point(1, 1);
+        Point point2 =  point1;
+        point1.x = 3;
+
+        int[][] numbers = { {1, 2, 3}, {4, 5, 6}};
+
+        System.out.println(Arrays.deepToString(numbers)); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public void sendEmail () {
+
+
+
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,3 @@
+public class test {
+    void sendEmail () {}
+}
